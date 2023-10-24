4cb27126c46a71cec748e96347326008490ab98a
rishsri <57784484+rishsri@users.noreply.github.com>
2023-10-24 12:18:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# logiccarts-assignment1
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/rishsri/logiccarts-assignment1)
---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <img
+            className="group"
+            alt="Group"
+            src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003358@2x.png"
+          />
+        </div>
+        <div className="overlap-group">
+          <div className="group-2">
+            <div className="overlap-group-2">
+              <div className="group-3">
+                <div className="text">Consignment booked</div>
+                <div className="text-wrapper">In Transit</div>
+                <div className="text-2">Out for delivery</div>
+                <div className="text-3">Delivered successfully</div>
+                <div className="rectangle" />
+              </div>
+              <div className="group-4" />
+              <div className="group-5" />
+              <div className="rectangle-2" />
+            </div>
+            <div className="overlap-2">
+              <img
+                className="ellipse"
+                alt="Ellipse"
+                src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/ellipse-1117.svg"
+              />
+              <img
+                className="img"
+                alt="Ellipse"
+                src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/ellipse-1118-1.svg"
+              />
+              <img
+                className="ellipse-2"
+                alt="Ellipse"
+                src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/ellipse-1119-1.svg"
+              />
+              <img
+                className="ellipse-3"
+                alt="Ellipse"
+                src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/ellipse-1120-1.svg"
+              />
+            </div>
+          </div>
+          <div className="text-wrapper-2">2</div>
+          <div className="text-wrapper-3">1</div>
+          <div className="text-wrapper-4">1</div>
+          <div className="text-wrapper-5">1</div>
+        </div>
+        <div className="overlap-3">
+          <div className="overlap-4">
+            <div className="rectangle-3" />
+            <div className="rectangle-4" />
+            <div className="rectangle-5" />
+            <div className="text-wrapper-6">Docket No</div>
+            <div className="text-wrapper-7">L123456789</div>
+            <div className="text-wrapper-8">L123456789</div>
+            <div className="text-wrapper-9">From</div>
+            <div className="text-wrapper-10">BLR</div>
+            <div className="text-wrapper-11">BLR</div>
+            <div className="text-wrapper-12">To</div>
+            <div className="text-wrapper-13">KER</div>
+            <div className="text-wrapper-14">KER</div>
+            <div className="text-wrapper-15">Status</div>
+            <div className="text-wrapper-16">Actions</div>
+            <img
+              className="line"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/line-2.svg"
+            />
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/line-3.svg"
+            />
+            <img
+              className="group-6"
+              alt="Group"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003368@2x.png"
+            />
+            <img
+              className="group-7"
+              alt="Group"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003368@2x.png"
+            />
+            <div className="frame">
+              <div className="text-wrapper-17">Delivered Successfully</div>
+            </div>
+            <div className="div-wrapper">
+              <div className="text-wrapper-17">Out for Delivery</div>
+            </div>
+          </div>
+          <div className="overlap-5">
+            <div className="rectangle-6" />
+            <div className="text-wrapper-18">L123456789</div>
+            <div className="text-wrapper-19">BLR</div>
+            <div className="text-wrapper-20">KER</div>
+            <img
+              className="group-8"
+              alt="Group"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003368@2x.png"
+            />
+            <div className="frame-2">
+              <div className="text-wrapper-17">Consignment Booked</div>
+            </div>
+          </div>
+          <div className="overlap-6">
+            <div className="rectangle-7" />
+            <div className="text-wrapper-21">L123456789</div>
+            <div className="text-wrapper-22">BLR</div>
+            <div className="text-wrapper-23">KER</div>
+            <img
+              className="group-9"
+              alt="Group"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003368@2x.png"
+            />
+            <div className="frame-3">
+              <div className="text-wrapper-17">In Transit</div>
+            </div>
+          </div>
+          <div className="overlap-7">
+            <div className="rectangle-8" />
+            <div className="text-wrapper-21">L123456789</div>
+            <div className="text-wrapper-22">BLR</div>
+            <div className="text-wrapper-23">KER</div>
+            <img
+              className="group-9"
+              alt="Group"
+              src="https://cdn.animaapp.com/projects/649d52e3d727f0241b9c98ea/releases/65375dc43cedcfda7bd9c33c/img/group-1000003368@2x.png"
+            />
+            <div className="frame-2">
+              <div className="text-wrapper-17">Consignment Booked</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
